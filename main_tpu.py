433d45ab2549829fae95a303523d365d5ee92ceb
Jesimon Barreto <jesimonbarreto@gmail.com>
2023-08-08 12:46:32 -0300

    adding image

--- main_tpu.py
+++ main_tpu.py
@@ -203,31 +203,142 @@ MODEL_OPTS = {
     },
 }
 
-FLAGS = args_parse.parse_common_options(
+def get_args_parser(datadir=None,
+                         logdir=None,
+                         num_cores=None,
+                         batch_size=128,
+                         num_epochs=10,
+                         num_workers=4,
+                         log_steps=20,
+                         lr=None,
+                         momentum=None,
+                         target_accuracy=None,
+                         profiler_port=9012,
+                         opts=None):
+    parser = argparse.ArgumentParser('DINO', add_help=False)
+
+    # Model parameters
+    parser.add_argument('--arch', default='vit_small', type=str,
+        choices=['vit_tiny', 'vit_small', 'vit_base', 'xcit', 'deit_tiny', 'deit_small'] \
+                + torchvision_archs + torch.hub.list("facebookresearch/xcit:main"),
+        help="""Name of architecture to train. For quick experiments with ViTs,
+        we recommend using vit_tiny or vit_small.""")
+    parser.add_argument('--patch_size', default=16, type=int, help="""Size in pixels
+        of input square patches - default 16 (for 16x16 patches). Using smaller
+        values leads to better performance but requires more memory. Applies only
+        for ViTs (vit_tiny, vit_small and vit_base). If <16, we recommend disabling
+        mixed precision training (--use_fp16 false) to avoid unstabilities.""")
+    parser.add_argument('--out_dim', default=65536, type=int, help="""Dimensionality of
+        the DINO head output. For complex and large datasets large values (like 65k) work well.""")
+    parser.add_argument('--norm_last_layer', default=True, type=utils.bool_flag,
+        help="""Whether or not to weight normalize the last layer of the DINO head.
+        Not normalizing leads to better performance but can make the training unstable.
+        In our experiments, we typically set this paramater to False with vit_small and True with vit_base.""")
+    parser.add_argument('--momentum_teacher', default=0.996, type=float, help="""Base EMA
+        parameter for teacher update. The value is increased to 1 during training with cosine schedule.
+        We recommend setting a higher value with small batches: for example use 0.9995 with batch size of 256.""")
+    parser.add_argument('--use_bn_in_head', default=False, type=utils.bool_flag,
+        help="Whether to use batch normalizations in projection head (Default: False)")
+
+    # Temperature teacher parameters
+    parser.add_argument('--warmup_teacher_temp', default=0.04, type=float,
+        help="""Initial value for the teacher temperature: 0.04 works well in most cases.
+        Try decreasing it if the training loss does not decrease.""")
+    parser.add_argument('--teacher_temp', default=0.04, type=float, help="""Final value (after linear warmup)
+        of the teacher temperature. For most experiments, anything above 0.07 is unstable. We recommend
+        starting with the default value of 0.04 and increase this slightly if needed.""")
+    parser.add_argument('--warmup_teacher_temp_epochs', default=0, type=int,
+        help='Number of warmup epochs for the teacher temperature (Default: 30).')
+
+    # Training/Optimization parameters
+    parser.add_argument('--use_fp16', type=utils.bool_flag, default=True, help="""Whether or not
+        to use half precision for training. Improves training time and memory requirements,
+        but can provoke instability and slight decay of performance. We recommend disabling
+        mixed precision if the loss is unstable, if reducing the patch size or if training with bigger ViTs.""")
+    parser.add_argument('--weight_decay', type=float, default=0.04, help="""Initial value of the
+        weight decay. With ViT, a smaller value at the beginning of training works well.""")
+    parser.add_argument('--weight_decay_end', type=float, default=0.4, help="""Final value of the
+        weight decay. We use a cosine schedule for WD and using a larger decay by
+        the end of training improves performance for ViTs.""")
+    parser.add_argument('--clip_grad', type=float, default=3.0, help="""Maximal parameter
+        gradient norm if using gradient clipping. Clipping with norm .3 ~ 1.0 can
+        help optimization for larger ViT architectures. 0 for disabling.""")
+    parser.add_argument('--batch_size_per_gpu', default=64, type=int,
+        help='Per-GPU batch-size : number of distinct images loaded on one GPU.')
+    parser.add_argument('--epochs', default=100, type=int, help='Number of epochs of training.')
+    parser.add_argument('--freeze_last_layer', default=1, type=int, help="""Number of epochs
+        during which we keep the output layer fixed. Typically doing so during
+        the first epoch helps training. Try increasing this value if the loss does not decrease.""")
+    parser.add_argument("--lr", default=0.0005, type=float, help="""Learning rate at the end of
+        linear warmup (highest LR used during training). The learning rate is linearly scaled
+        with the batch size, and specified here for a reference batch size of 256.""")
+    parser.add_argument("--warmup_epochs", default=10, type=int,
+        help="Number of epochs for the linear learning-rate warm up.")
+    parser.add_argument('--min_lr', type=float, default=1e-6, help="""Target LR at the
+        end of optimization. We use a cosine LR schedule with linear warmup.""")
+    parser.add_argument('--optimizer', default='adamw', type=str,
+        choices=['adamw', 'sgd', 'lars'], help="""Type of optimizer. We recommend using adamw with ViTs.""")
+    parser.add_argument('--drop_path_rate', type=float, default=0.1, help="stochastic depth rate")
+
+    # Multi-crop parameters
+    parser.add_argument('--global_crops_scale', type=float, nargs='+', default=(0.4, 1.),
+        help="""Scale range of the cropped image before resizing, relatively to the origin image.
+        Used for large global view cropping. When disabling multi-crop (--local_crops_number 0), we
+        recommand using a wider range of scale ("--global_crops_scale 0.14 1." for example)""")
+    parser.add_argument('--local_crops_number', type=int, default=8, help="""Number of small
+        local views to generate. Set this parameter to 0 to disable multi-crop training.
+        When disabling multi-crop we recommend to use "--global_crops_scale 0.14 1." """)
+    parser.add_argument('--local_crops_scale', type=float, nargs='+', default=(0.05, 0.4),
+        help="""Scale range of the cropped image before resizing, relatively to the origin image.
+        Used for small local view cropping of multi-crop.""")
+
+    # Misc
+    parser.add_argument('--data_path', default='/path/to/imagenet/train/', type=str,
+        help='Please specify path to the ImageNet training data.')
+    parser.add_argument('--output_dir', default=".", type=str, help='Path to save logs and checkpoints.')
+    parser.add_argument('--saveckp_freq', default=20, type=int, help='Save checkpoint every x epochs.')
+    parser.add_argument('--seed', default=0, type=int, help='Random seed.')
+    parser.add_argument('--num_workers', default=10, type=int, help='Number of data loading workers per GPU.')
+    parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up
+        distributed training; see https://pytorch.org/docs/stable/distributed.html""")
+    parser.add_argument("--local_rank", default=0, type=int, help="Please ignore and do not set this argument.")
+    parser.add_argument('--datadir', type=str, default=datadir)
+    parser.add_argument('--logdir', type=str, default=logdir)
+    parser.add_argument('--num_cores', type=int, default=num_cores)
+    parser.add_argument('--batch_size', type=int, default=batch_size)
+    parser.add_argument('--num_epochs', type=int, default=num_epochs)
+    parser.add_argument('--num_workers', type=int, default=num_workers)
+    parser.add_argument('--log_steps', type=int, default=log_steps)
+    parser.add_argument('--profiler_port', type=int, default=profiler_port)
+    parser.add_argument('--lr', type=float, default=lr)
+    parser.add_argument('--momentum', type=float, default=momentum)
+    parser.add_argument('--target_accuracy', type=float, default=target_accuracy)
+    parser.add_argument('--drop_last', action='store_true')
+    parser.add_argument('--fake_data', action='store_true')
+    parser.add_argument('--tidy', action='store_true')
+    parser.add_argument('--metrics_debug', action='store_true')
+    parser.add_argument('--async_closures', action='store_true')
+    parser.add_argument('--debug', action='store_true')
+    if opts:
+    for name, aopts in opts:
+        parser.add_argument(name, **aopts)
+    args, leftovers = parser.parse_known_args()
+    sys.argv = [sys.argv[0]] + leftovers
+    # Setup import folders.
+    xla_folder = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
+    sys.path.append(os.path.join(os.path.dirname(xla_folder), 'test'))
+    
+    
+    return parser
+
+FLAGS = get_args_parser(
     datadir='~/imagenet',
-    patch_size=None,
-    out_dim=None,
-    norm_last_layer=None,
-    momentum_teacher=None,
-    use_bn_in_head=None,
     batch_size=None,
     num_epochs=None,
     momentum=None,
     lr=None,
-    warmup_teacher_temp=None,
-    teacher_temp=None,
-    warmup_teacher_temp_epochs=None,
     target_accuracy=None,
     profiler_port=9012,
-    local_crops_number=None,
-    global_crops_scale=None,
-    local_crops_scale=None,
-    output_dir=None,
-    saveckp_freq=None,
-    num_workers=None,
-    dist_url=None,
-    local_rank=None,
-    fp16_scaler=None,
     opts=MODEL_OPTS.items(),
 )
 
