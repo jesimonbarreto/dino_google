42d32fac89283f287de3aeda8321f4369041c999
Jesimon Barreto <jesimonbarreto@github.com>
2023-08-19 20:05:12 -0300

    Updating main_tpu

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -22,6 +22,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributed as dist
 import torch.optim as optim
 from torchvision import datasets, transforms
 import torch_xla
@@ -43,7 +44,108 @@ torchvision_archs = sorted(name for name in torchvision_models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(torchvision_models.__dict__[name]))
 
+class DINOLoss(nn.Module):
+    def __init__(self, out_dim, ncrops, warmup_teacher_temp, teacher_temp,
+                 warmup_teacher_temp_epochs, nepochs, student_temp=0.1,
+                 center_momentum=0.9):
+        super().__init__()
+        self.student_temp = student_temp
+        self.center_momentum = center_momentum
+        self.ncrops = ncrops
+        self.register_buffer("center", torch.zeros(1, out_dim))
+        # we apply a warm up for the teacher temperature because
+        # a too high temperature makes the training instable at the beginning
+        self.teacher_temp_schedule = np.concatenate((
+            np.linspace(warmup_teacher_temp,
+                        teacher_temp, warmup_teacher_temp_epochs),
+            np.ones(nepochs - warmup_teacher_temp_epochs) * teacher_temp
+        ))
+    
+    @torch.no_grad()
+    def update_center(self, teacher_output):
+        """
+        Update center used for teacher output.
+        """
+        batch_center = torch.sum(teacher_output, dim=0, keepdim=True)
+        dist.all_reduce(batch_center)
+        batch_center = batch_center / (len(teacher_output) * dist.get_world_size())
 
+        # ema update
+        self.center = self.center * self.center_momentum + batch_center * (1 - self.center_momentum)
+
+    def forward(self, student_output, teacher_output, epoch):
+        """
+        Cross-entropy between softmax outputs of the teacher and student networks.
+        """
+        student_out = student_output / self.student_temp
+        student_out = student_out.chunk(self.ncrops)
+
+        # teacher centering and sharpening
+        temp = self.teacher_temp_schedule[epoch]
+        teacher_out = F.softmax((teacher_output - self.center) / temp, dim=-1)
+        teacher_out = teacher_out.detach().chunk(2)
+
+        total_loss = 0
+        n_loss_terms = 0
+        for iq, q in enumerate(teacher_out):
+            for v in range(len(student_out)):
+                if v == iq:
+                    # we skip cases where student and teacher operate on the same view
+                    continue
+                loss = torch.sum(-q * F.log_softmax(student_out[v], dim=-1), dim=-1)
+                total_loss += loss.mean()
+                n_loss_terms += 1
+        total_loss /= n_loss_terms
+        self.update_center(teacher_output)
+
+        return total_loss
+
+class DataAugmentationDINO(object):
+    def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
+        flip_and_color_jitter = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+        ])
+        normalize = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+
+        # first global crop
+        self.global_transfo1 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            utils.GaussianBlur(1.0),
+            normalize,
+        ])
+        # second global crop
+        self.global_transfo2 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            utils.GaussianBlur(0.1),
+            utils.Solarization(0.2),
+            normalize,
+        ])
+        # transformation for the local small crops
+        self.local_crops_number = local_crops_number
+        self.local_transfo = transforms.Compose([
+            transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            utils.GaussianBlur(p=0.5),
+            normalize,
+        ])
+
+    def __call__(self, image):
+        crops = []
+        crops.append(self.global_transfo1(image))
+        crops.append(self.global_transfo2(image))
+        for _ in range(self.local_crops_number):
+            crops.append(self.local_transfo(image))
+        return crops
 
 class MNIST(nn.Module):
 
@@ -138,27 +240,39 @@ def train_mnist(flags,
   lr = flags.lr * xm.xrt_world_size()
 
   device = xm.xla_device()
-  model = vits.__dict__[FLAGS.arch](
+  student = vits.__dict__[FLAGS.arch](
             patch_size=FLAGS.patch_size,
             drop_path_rate=FLAGS.drop_path_rate,  # stochastic depth
         ).to(device)
+  teacher = vits.__dict__[FLAGS.arch](patch_size=FLAGS.patch_size).to(device)
+  embed_dim = student.embed_dim
   writer = None
   if xm.is_master_ordinal():
     writer = test_utils.get_summary_writer(flags.logdir)
-  optimizer = optim.SGD(model.parameters(), lr=lr, momentum=flags.momentum)
-  loss_fn = nn.NLLLoss()
+  optimizer = optim.SGD(student.parameters(), lr=lr, momentum=flags.momentum)
+  
+  dino_loss = DINOLoss(
+        FLAGS.out_dim,
+        FLAGS.local_crops_number + 2,  # total number of crops = 2 global crops + local_crops_number
+        FLAGS.warmup_teacher_temp,
+        FLAGS.teacher_temp,
+        FLAGS.warmup_teacher_temp_epochs,
+        FLAGS.epochs,
+  )
 
   server = xp.start_server(flags.profiler_port)
 
   def train_loop_fn(loader):
     tracker = xm.RateTracker()
-    model.train()
+    student.train()
+    teacher.train()
     for step, (data, target) in enumerate(loader):
       with xp.StepTrace('train_mnist', step_num=step):
         with xp.Trace('build_graph'):
           optimizer.zero_grad()
-          output = model(data)
-          loss = loss_fn(output, target)
+          s_out = student(data)
+          t_out = teacher(data[:2])
+          loss = dino_loss(s_out, t_out, step)
           loss.backward()
         xm.optimizer_step(optimizer)
         if fetch_often:
@@ -172,10 +286,10 @@ def train_mnist(flags,
   def test_loop_fn(loader):
     total_samples = 0
     correct = 0
-    model.eval()
+    student.eval()
     for data, target in loader:
       with xp.StepTrace('test_mnist'):
-        output = model(data)
+        output = student(data)
         pred = output.max(1, keepdim=True)[1]
         correct += pred.eq(target.view_as(pred)).sum()
         total_samples += data.size()[0]
